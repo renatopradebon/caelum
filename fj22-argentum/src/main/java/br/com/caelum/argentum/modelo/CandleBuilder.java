39e7a9d2f11f9a41d70c85d09ec575b2b122ff57
Renato Pradebon <renatopradebon@hotmail.com>
2016-02-17 21:04:17 -0200

    Finalizando capitulo 3 com os testes do CandleBuilder

--- fj22-argentum/src/main/java/br/com/caelum/argentum/modelo/CandleBuilder.java
+++ fj22-argentum/src/main/java/br/com/caelum/argentum/modelo/CandleBuilder.java
@@ -11,38 +11,72 @@ public class CandleBuilder {
 	private double volume;
 	private Calendar data;
 	
+	private boolean chamouAbertura;
+	private boolean chamouFechamento;
+	private boolean chamouMinimo;
+	private boolean chamouMaximo;
+	private boolean chamouVolume;
+	private boolean chamouData;
+	
+	public CandleBuilder() {
+		chamouAbertura = false;
+		chamouFechamento = false;
+		chamouMinimo = false;
+		chamouMaximo = false;
+		chamouVolume = false;
+		chamouData = false;
+	}
+	
 	public CandleBuilder comAbertura(double abertura) {
 		this.abertura = abertura;
+		chamouAbertura = true;
 		return this;
 	}
 	
 	public CandleBuilder comFechamento(double fechamento) {
 		this.fechamento = fechamento;
+		chamouFechamento = true;
 		return this;
 	}
 	
 	public CandleBuilder comMinimo(double minimo) {
 		this.minimo = minimo;
+		chamouMinimo = true;
 		return this;
 	}
 	
 	public CandleBuilder comMaximo(double maximo) {
 		this.maximo = maximo;
+		chamouMaximo = true;
 		return this;
 	}
 	
-	public CandleBuilder comVolume(double volume) {
+	public CandleBuilder comVolume(double volume) {		
 		this.volume = volume;
+		chamouVolume = true;
 		return this;
 	}
 	
 	public CandleBuilder comData(Calendar data) {
 		this.data = data;
+		chamouData = true;
 		return this;
 	}
 	
-	public Candlestick geraCandle() {
-		return new Candlestick(abertura, fechamento, minimo, maximo,
-		volume, data);
+	public boolean verificaSeChamouTodosOsMetodos() {
+		return chamouAbertura && chamouData 
+				&& chamouFechamento && chamouMaximo 
+				&& chamouMinimo && chamouVolume ? true : false;
 	}
+	
+	public Candlestick geraCandle() { 
+		
+		if (verificaSeChamouTodosOsMetodos() == false) {			
+			throw new IllegalStateException("Obrigatório passar todos os parâmetros.");
+		} else {
+			return new Candlestick(abertura, fechamento, minimo, maximo,
+					volume, data);
+		}
+	}
+		
 }
