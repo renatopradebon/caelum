8c0f1022f7604dad00507766a81d9a141264836e
Renato Pradebon <renatopradebon@hotmail.com>
2016-02-25 12:41:29 -0300

    Adicionando classe SerieTemporal

--- fj-22-argentum-web/src/main/java/br/com/caelum/argentum/modelo/SerieTemporal.java
+++ fj-22-argentum-web/src/main/java/br/com/caelum/argentum/modelo/SerieTemporal.java
@@ -3,7 +3,6 @@ package br.com.caelum.argentum.modelo;
 import java.util.List;
 
 public class SerieTemporal {
-	
 	private final List<Candlestick> candles;
 
 	public SerieTemporal(List<Candlestick> candles) {
@@ -13,8 +12,8 @@ public class SerieTemporal {
 	public Candlestick getCandle(int i) {
 		return this.candles.get(i);
 	}
-	
+
 	public int getUltimaPosicao() {
 		return this.candles.size() - 1;
-	}	
+	}
 }
